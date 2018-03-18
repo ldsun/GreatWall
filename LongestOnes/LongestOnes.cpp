02343519e478a8166bae97187d85058ce4529918
ldsun <sd4ssun@gmail.com>
2018-03-18 00:03:13 -0700

    update

--- LongestOnes/LongestOnes.cpp
+++ LongestOnes/LongestOnes.cpp
@@ -5,46 +5,45 @@ using namespace std;
 
 void findLongestOnes(vector <int> input, int *start, int *end, int k)
 {
-    int s = 0, e = 0, remain_zeros = k, length = 0, next = 0, max_length = 0;
+    int s = 0, e = 0;
+    int remain_zeros = k;
 
-    // Scan to find first 1
-    while (input[s++] == 0); s--;
-
-    //iterate the array using next index. 
-    next = s + 1;
-    length = 1;
-    while (next < input.size()) {
-        // if next is 1
-        if (input[next] == 1) {
-            e = next;
-            length++;
-            next++;
-        } else {
-            if (remain_zeros > 0) {
-                remain_zeros--;
-                next++;
-            } else {
-                if (length > max_length) {
-                    max_length = length;
-                    *start = s;
-                    *end = e;
+    while (e < input.size()) {
+        // Update start index
+        // When to update start index:
+        // 1. Initial state
+        // 2. No more 0 to remove
+        if (remain_zeros == k) {
+            // if e < input.size() and remain_zeros == k, then it's initial state:
+            //Initial state: remove leading 0s
+            while (input[s++] == 0); s--;
+            e = s;
+            cout << "Initial start index is: " << s << endl;
+        } else if (remain_zeros == 0) {
+            while (input[++s] == 1);
+            while (input[s] == 0) {
+                remain_zeros++;
+                s++;
+                if (remain_zeros > k) {
+                    cout << "Error: remain_zeros > k" << endl;
                 }
-                while (input[++s] == 1);
-                while (input[s] == 0) {
-                    remain_zeros++;
-                    s++;
-                }
-
-            }
+            }   
         }
+
+        // Update end index
+        // When to update end index:
+        // 1. remain_zeros > 0
+        while (remain_zeros > 0 && e < input.size()) {
+            if (input[e+1] == 0) {
+                remain_zeros--;
+            }
+            e++;
+        }
+        while (e < input.size() && input[++e] == 1);
+        if (e < input.size()) {
+            e--;
+        }
+
     }
 
-    if (length > max_length) {
-        max_length = length;
-        *start = s;
-        *end = e;
-    }
-
-    *start = s;
-    *end = e;
 }
--- LongestOnes/notes.md
+++ LongestOnes/notes.md
@@ -1,3 +1,9 @@
 Question is: 
 
-Given an array consists of only 0 and 1, and they are scattered randomly. How to remove 0 at most K times, so that you have a longest subarray of 1s. 
+Given an array consists of only 0 and 1, and they are scattered randomly. How to remove 0 at most K times, so that you have a longest subarray of 1s, point out the start index and end index of that subarray. 
+
+Analysis:
+1. All removed Os can't have 0s that are not removed in between. 
+2. When updating start index, need to cross over at least one 0.
+3. When updating end index, as long as there's available 0s, you can move to next 0. If there's no available 0 to remove, stop before next 0. 
+4. When calculating the length of 1s: end index - start index + 1 - # of removed 0s
