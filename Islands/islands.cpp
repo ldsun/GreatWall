b1a84b8c5eb7918782bf6bf9e4a553b861acbc7b
ldsun <sd4ssun@gmail.com>
2018-03-22 22:27:19 -0700

    added

--- Islands/islands.cpp
+++ Islands/islands.cpp
@@ -3,17 +3,21 @@
 
 using namespace std;
 
-void bfs(const vector < vector <int> > &input, vector < vector <bool> > &visited, int i, int j)
+void bfs(const vector < vector <int> > &input, vector < vector <int> > &visited, int i, int j, int num)
 {
     if (i < 0 || i >= input.size() || j < 0 || j >= input[0].size()) return;
 
-    if (visited[i][j] == false) {
-        visited[i][j] = true;
-        if (input[i][j] == 0) return;
-        bfs(input, visited, i-1, j);
-        bfs(input, visited, i, j-1);
-        bfs(input, visited, i+1, j);
-        bfs(input, visited, i, j+1);
+    if (visited[i][j] == -1) {
+        
+        if (input[i][j] == 0) {
+            visited[i][j] = 0;
+            return;
+        }
+        visited[i][j] = num;
+        bfs(input, visited, i-1, j, num);
+        bfs(input, visited, i, j-1, num);
+        bfs(input, visited, i+1, j, num);
+        bfs(input, visited, i, j+1, num);
     }
 }
 
@@ -24,18 +28,26 @@ int countIslands(vector < vector<int> > &input)
     if (m == 0) return 0;
     const int n = input[0].size();
     if (n == 0) return 0;
-    vector < vector <bool> > visited {input.size(), vector<bool>(input[0].size(), false)};
+    vector < vector <int> > visited {input.size(), vector<int>(input[0].size(), -1)};
 
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
-            if (input[i][j] == 0 && visited[i][j] == false) {
-                visited[i][j] = true;
+            if (input[i][j] == 0 && visited[i][j] == -1) {
+                visited[i][j] = 0;
                 continue;
-            } else if (visited[i][j] == false) {
+            } else if (visited[i][j] == -1) {
                 num++;
-                bfs(input, visited, i, j);
+                bfs(input, visited, i, j, num);
             }
         }
+    }
+
+    cout << "Island marker: " << endl;
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            cout << visited[i][j] << " ";
+        }
+        cout << endl;
     }
     return num;
 }
